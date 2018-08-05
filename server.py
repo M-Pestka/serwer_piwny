b42467c5d64c53b456a1be85590b2960ca998cb4
kubasikora <J.Sikora@stud.elka.pw.edu.pl>
2018-08-05 16:12:58 +0200

    port configuration

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from api import api_docs, bicycles
 from flask_cors import CORS
+import os
 
 b_server = Flask(__name__)
 CORS(b_server)
@@ -19,4 +20,5 @@ if __name__ == '__main__':
     b_server.config['RESTFUL_JSON'] = {
             'ensure_ascii': False
     }
-    b_server.run()
+    port = os.getenv('PORT', 5000)
+    b_server.run(port=port)
